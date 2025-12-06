7bb2e040f0567720c282c2fb352d3e38eabd3e80
Gabriel Gonzalez <gabrielenrique.gonzalez97@gmail.com>
2025-12-06 15:59:51 -0300

    feat: add AI Label component
    Signed-off-by: Gabriel Gonzalez <gabrielenrique.gonzalez97@gmail.com>

--- frontend/src/lib/common/index.ts
+++ frontend/src/lib/common/index.ts
@@ -1,6 +1,7 @@
 export * from "./constants";
 
+import AILabel from "./AILabel.svelte";
 import LoadingComponent from "./LoadingComponent.svelte";
 import Tabs from "./Tabs.svelte";
 
-export { LoadingComponent, Tabs };
+export { AILabel, LoadingComponent, Tabs };
