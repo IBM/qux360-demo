4e45e321a4e9c6570e15971579f1c7698e9c165c
Gabriel Gonzalez <gabrielenrique.gonzalez97@gmail.com>
2026-06-15 23:06:29 -0300

    refactor(tabs): replace custom tab implementation with Carbon Tabs component

--- frontend/src/lib/common/Tabs.svelte
+++ frontend/src/lib/common/Tabs.svelte
@@ -1,56 +1,23 @@
 <script lang="ts">
     import type { TabI } from "$lib/models";
+    import { Tab, Tabs } from "carbon-components-svelte";
 
     export let tabs: TabI[];
     export let activeTab: string;
 
-    const selectTab = (id: string): void => {
-        activeTab = id;
+    const handleChange = (e: CustomEvent<number>): void => {
+        const tab = tabs[e.detail];
+        if (tab) {
+            activeTab = tab.id;
+        }
     };
 </script>
 
-<div class="tabs-container">
-    <ul class="bx--tabs__nav bx--tabs__nav--hidden">
-        {#each tabs as tab (tab.id)}
-            <li class="bx--tabs__nav-item">
-                <a
-                    role="tab"
-                    tabindex="0"
-                    aria-selected={activeTab === tab.id}
-                    aria-disabled="false"
-                    class="bx--tabs__nav-link {activeTab === tab.id
-                        ? 'bx--tabs__nav-item--selected'
-                        : ''}"
-                    on:click={() => selectTab(tab.id)}
-                >
-                    {tab.label}
-                </a>
-            </li>
-        {/each}
-    </ul>
-</div>
-
-<style lang="scss">
-    .tabs-container {
-        position: relative;
-        min-height: calc(2.5rem - 0.5px);
-        border-bottom: 1px solid var(--cds-border-subtle-selected);
-    }
-
-    .bx--tabs__nav-link {
-        width: auto;
-        border-color: var(--cds-border-subtle-selected);
-    }
-
-    .bx--tabs__nav-link:active,
-    .bx--tabs__nav-link:focus {
-        width: auto;
-        outline: unset;
-        outline-offset: unset;
-        border-bottom: 2px solid var(--cds-interactive-04);
-    }
-
-    .bx--tabs__nav-item--selected {
-        border-bottom: 2px solid var(--cds-interactive-04);
-    }
-</style>
+<Tabs
+    selected={tabs.findIndex((t) => t.id === activeTab)}
+    on:change={handleChange}
+>
+    {#each tabs as tab (tab.id)}
+        <Tab label={tab.label} />
+    {/each}
+</Tabs>
